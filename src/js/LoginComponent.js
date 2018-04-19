a2d5c7246dedbe28e77169f888015d8c25a8c087
gfalbarracinr <gfalbarracinr@unal.edu.co>
2018-04-19 03:10:42 -0500

    con registro de usuarios 80%

--- src/js/LoginComponent.js
+++ src/js/LoginComponent.js
@@ -7,7 +7,7 @@ import AppHomeComponent from './AppHomeComponent.js'
 import ReactDOM from 'react-dom';
 import store from './store'
 import axios from 'axios';
-import swal from 'sweetalert'
+import SweetAlert from 'sweetalert2-react';
 
 class LoginComponent extends React.Component {
   constructor(props){
@@ -18,7 +18,8 @@ class LoginComponent extends React.Component {
       role:'users',
       token: '',
       redirect: false,
-      isLoading: false
+      isLoading: false,
+      error: false
     };
 
     store.subscribe(() => {
@@ -86,10 +87,10 @@ class LoginComponent extends React.Component {
                     
                   })
                   .catch((error) =>{
-                    swal ( "Error" ,  "correo o contraseña incorrecta" ,  "error" )
+                    {/*swal ( "Error" ,  "correo o contraseña incorrecta" ,  "error" )*/}
                     console.log('Failed miserably :(')
                     console.log(error)
-                    this.setState({isLoading: false})
+                    this.setState({isLoading: false, error: true})
                   })
 
     }
@@ -151,6 +152,14 @@ class LoginComponent extends React.Component {
      }
     console.log(this.state.token)
     var match = { params:{section:''}};
+    if (this.state.error){
+      return (<SweetAlert
+        show = {this.state.error}
+        title="Error"
+        text="Error, correo o contraseña incorrecta"
+        onConfirm={() => this.setState({ error: false })}
+      />);
+    }
     if (this.state.isLoading){
       return (
         this.state.token != '' ?
