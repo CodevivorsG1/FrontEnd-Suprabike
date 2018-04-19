f16966f588190eea11bea0407e3656419f050cfc
Unknown <diefrodriguezcha@unal.edu.co>
2018-04-19 10:38:38 -0500

    Cambiado a react-pdf, solo renderiza una pagina

--- src/js/PDF_Components/PDFViewer.js
+++ src/js/PDF_Components/PDFViewer.js
@@ -1,61 +1,31 @@
 import React from 'react';
-import PDF from 'react-pdf-js';
+import testPDF from "./2010.Echeverri&Pereira.Mambear coca.español.pdf";
+import { Document, Page } from 'react-pdf';
 
 class MyPdfViewer extends React.Component {
-  state = {};
-
-  onDocumentComplete = (pages) => {
-    this.setState({ page: 1, pages });
+  state = {
+    numPages: null,
+    pageNumber: 1,
   }
 
-  onPageComplete = (page) => {
-    this.setState({ page });
-  }
-
-  handlePrevious = () => {
-    this.setState({ page: this.state.page - 1 });
-  }
-
-  handleNext = () => {
-    this.setState({ page: this.state.page + 1 });
-  }
-
-  renderPagination = (page, pages) => {
-    let previousButton = <li className="previous" onClick={this.handlePrevious}><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    if (page === 1) {
-      previousButton = <li className="previous disabled"><a href="#"><i className="fa fa-arrow-left"></i> Previous</a></li>;
-    }
-    let nextButton = <li className="next" onClick={this.handleNext}><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    if (page === pages) {
-      nextButton = <li className="next disabled"><a href="#">Next <i className="fa fa-arrow-right"></i></a></li>;
-    }
-    return (
-      <nav>
-        <ul className="pager">
-          {previousButton}
-          {nextButton}
-        </ul>
-      </nav>
-      );
+  onDocumentLoad = ({ numPages }) => {
+    this.setState({ numPages });
   }
 
   render() {
-    let pagination = null;
-    if (this.state.pages) {
-      pagination = this.renderPagination(this.state.page, this.state.pages);
-    }
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div>
-        <PDF
-          file="somefile.pdf"
-          onDocumentComplete={this.onDocumentComplete}
-          onPageComplete={this.onPageComplete}
-          page={this.state.page}
-        />
-        {pagination}
+        <Document
+          file={testPDF}
+          onLoadSuccess={this.onDocumentLoad}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
       </div>
-    )
+    );
   }
 }
-
-module.exports = MyPdfViewer;
+export default MyPdfViewer;
