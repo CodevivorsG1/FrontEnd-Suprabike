8250417487fbf7ec0cf90d6fe732fcdac16dd389
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-01 11:22:30 -0500

    Añadido paquete google-map-react

--- src/js/Map_Components/MapComponent.js
+++ src/js/Map_Components/MapComponent.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
 
 
-class MapComponent extends React.Component{
+class SimpleMap extends React.Component{
+
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lon: 30.33
+        },
+        zoom: 11
+    };
+
     render(){
         return({
-            
+            <div style={{height:'100%', width:'100%'}}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{key: AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A}}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent 
+                        lat={59.955413}
+                        lng={30.337844}
+                        text={'Kreyser Avrora'}
+                    />
+                <GoogleMapReact />
+            </div>            
         });
     }
 }
 
-export default MapComponent;
+export default SimpleMap;
