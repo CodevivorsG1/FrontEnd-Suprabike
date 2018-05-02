4e8a25520c7f63b34c8e5b0c0822b7aaba308173
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-02 09:35:25 -0500

    Probando con map

--- src/js/Map_Components/MapComponent.js
+++ src/js/Map_Components/MapComponent.js
@@ -1,28 +1,78 @@
 import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import axios from 'axios';
 
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
     this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.getGeocode = this.getGeocode.bind(this);
+
+
     this.state = {
       showingInfoWindow: false,
       activeMarker: {},
-      selectedPlace: {}
+      selectedPlace: {},
+      storesAddresses: []
     };
   }
   onMarkerClick(props, marker, e) {
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
-      showingInfoWindow: true
+      showingInfoWindow: true,
+      storesAddresses: [],
+      storesMarkers: []
     });
   }
+
+  getGeocode(url){
+    axios.get(url)
+        .then((response) => {
+            console.log(response.data.results);
+            let data = response.data.results[0].geometry.location;
+            this.state.storesAddresses.push(data);
+            //this.state.storesMarkers.push(<Marker position={data}/>)
+            //console.log(this.state.storesMarkers);
+        })
+        .catch((error) => {
+            console.log("No data found!");
+        })
+
+  } 
+
+  componentWillMount(){
+    axios.get("http://localhost:4000/stores")
+        .then((response) => {
+            //console.log(response);
+            for(var x in response.data){    // Obtiene todas las direcciones del pedido
+                let record = response.data[x]
+                let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + record.address_store +",Bogotá&key=AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A";                    
+                this.getGeocode(url);
+            }
+            console.log(this.state.storesAddresses);
+        })
+        .catch((error) => {
+            console.log("Nothing happened");
+        })
+}
+
+ /* loadMarkers = ({list}) => {
+    if(this.props.google){
+      return(
+        list.map(item =>
+          <Marker position={item}/>
+        )
+      )
+    }
+  }*/
+
   render() {
     if (!this.props.google) {
       return <div>Loading...</div>;
     }
-
+    console.log("Ready? ");
+    
     return (
       <div
         style={{
@@ -38,19 +88,15 @@ export class MapContainer extends Component {
                 lng: -74.101081
             }}
         >
-          <Marker
-            onClick={this.onMarkerClick}
-            position={{lat: 4.649737,lng: -74.101081}}            
-            name={"Current location"}
-          />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-          >
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-          </InfoWindow>
+          {
+            this.state.storesAddresses.map((item, i) => {
+              console.log(i)
+              return(
+                <Marker key={i} position={item}/>
+              )
+            })
+          }     
+          <Marker position={this.state.storesAddresses[6]}/>
         </Map>
       </div>
     );
