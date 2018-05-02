45660f91cd8d2e7854b5c6da9895fadd73e371cf
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-01 19:52:27 -0500

    Cambio de paquete: google-maps-react

--- src/js/Map_Components/MapComponent.js
+++ src/js/Map_Components/MapComponent.js
@@ -1,83 +1,61 @@
-import React from 'react';
-import {withGoogleMap, withScriptjs, GoogleMap, Marker} from 'react-google-maps';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
-
-class SimpleMap extends React.Component{
-    constructor(){
-        super();
-        this.fillMarkers = this.fillMarkers.bind(this);
-        this.getGeocode = this.getGeocode.bind(this);
-
-        this.state = {
-            storesAddresses:[],
-            storesMarkers:[]
-        }
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  render() {
+    if (!this.props.google) {
+      return <div>Loading...</div>;
     }
 
-    fillMarkers(){        
-        for(var x in this.state.storesAddresses){
-            let address = this.state.storesAddresses[x];
-            let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address +",Bogotá&key=AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A";
-            axios.get(url)
-                .then((response) => {
-                    console.log(response);
-                })
-                .catch((error) => {
-                    console.log("Fuck, we failed");
-                })
-        }
-    }
-
-    getGeocode(url){
-        axios.get(url)
-            .then((response) => {
-                let data = response.data.results[0].geometry.location;
-                this.state.storesAddresses.push(data);
-            })
-            .catch((error) => {
-                console.log("No data found!");
-            })
-
-    }
-
-    componentWillMount(){
-        axios.get("http://localhost:4000/stores")
-            .then((response) => {
-                //console.log(response);
-                for(var x in response.data){    // Obtiene todas las direcciones del pedido
-                    let record = response.data[x]
-                    let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + record.address_store +",Bogotá&key=AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A";                    
-                    this.getGeocode(url);
-                }
-            })
-            .catch((error) => {
-                console.log("Nothing happened");
-            })
-    }
-
-    render(){
-
-        const GoogleMapExample = withScriptjs(withGoogleMap(props => (
-            <GoogleMap
-              defaultCenter = { { lat: 4.639428, lng: -74.084143 } }
-              defaultZoom = { 13 }
-            >            
-            </GoogleMap>
-         )));
-
-        return(
+    return (
+      <div
+        style={{
+          position: "relative",
+          height: "calc(100vh - 60px)"
+        }}>
+        <h1>This Fucking Map</h1>
+        <Map style={{}}
+            google={this.props.google}
+            zoom={14}
+            initialCenter={{
+                lat: 4.649737,
+                lng: -74.101081
+            }}
+        >
+          <Marker
+            onClick={this.onMarkerClick}
+            position={{lat: 4.649737,lng: -74.101081}}            
+            name={"Current location"}
+          />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+          >
             <div>
-                <h1>This is a map!</h1>
-                <GoogleMapExample
-                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A"
-                    loadingElement={<div style={{ height: `100%` }} />}
-                    containerElement={ <div style={{ height: `500px`, width: '100%' }} /> }
-                    mapElement={ <div style={{ height: `100%` }} /> }
-                />
+              <h1>{this.state.selectedPlace.name}</h1>
             </div>
-        );
-    }
+          </InfoWindow>
+        </Map>
+      </div>
+    );
+  }
 }
-
-export default SimpleMap;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A"
+})(MapContainer);
