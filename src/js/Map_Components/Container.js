979ac3c905cbe5ee893618b8a54ad91d4683cb6c
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-03 03:16:29 -0500

    Nuevo intento, no funciona aun :(

--- src/js/Map_Components/Container.js
+++ src/js/Map_Components/Container.js
@@ -1,31 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import axios from 'axios';
+import {GoogleApiWrapper} from 'google-maps-react';
+import { Route } from 'react-router-dom';
+import Map from './Map.js';
 
 export class MapContainer extends React.Component {
 
-    render() {      
-    return (
-      <div
-        style={{
-          position: "relative",
-          height: "500px",
-          width: "100%"
-        }}
-      >
-        < Map google={this.props.google}
-        zoom={14}
-        initialCenter={{
-          lat: 4.649737,
-          lng: -74.101081
-          }}
-        >
+    state = {
+      addresses: []
+    }
 
-          < Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />       
-        </ Map>
-      </div>
-    );
+    getGeocode = (url) => {
+      axios.get(url)
+          .then((response) => {
+              let data = response.data.results[0].geometry.location;
+              this.state.addresses.push(data);              
+              //console.log(this.state.storesMarkers);
+          })
+          .catch((error) => {
+              console.log("No data found!");
+          })  
+    };
+
+    componentDidMount(){
+      axios.get("http://localhost:4000/stores")
+        .then((response) => {
+            //console.log(response);
+            for(var x in response.data){    // Obtiene todas las direcciones del pedido
+                let record = response.data[x]
+                let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + record.address_store +",Bogotá&key=AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A";                    
+                this.getGeocode(url);
+            }
+            console.log(this.state.addresses);
+        })
+        .catch((error) => {
+            console.log("Nothing happened");
+        })
+    }
+
+    render() {      
+      return (
+        <div className="MapContainer">
+          <div className="wrapper">
+            <Route path='/map' render={(props) => < Map google={this.props.google} addresses={this.state.addresses} {...props}/>} />
+          </div>
+        </div>
+      );
   }
 }
 
--- src/js/Map_Components/Map.js
+++ src/js/Map_Components/Map.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export class Map extends React.Component {
-    componentDidMount(){
-        this.loadMap();
-    }
+export default class Map extends React.Component {
 
-    componentDidUpdate(prevProps, prepState){
-        if (prevProps.google !== this.props.google) {
-            this.loadMap();
-          }
-    }
+    componentDidUpdate(prevProps, prevState) {
+      if (prevProps.google !== this.props.google || prevProps.addresses !== this.props.addresses) {
+        this.loadMap();
+      }
+    }   
 
     loadMap(){
         if (this.props && this.props.google) {
@@ -21,21 +18,29 @@ export class Map extends React.Component {
             const mapRef = this.refs.map;
             const node = ReactDOM.findDOMNode(mapRef);
 
-            let zoom = 14;
-            let lat = 37.774929;
-            let lng = -122.419416;
-            const center = new maps.LatLng(lat, lng);
             const mapConfig = Object.assign({}, {
-                center: center,
-                zoom: zoom
+              center: {lat:0, lng:180},
+              zoom: 2              
             })
+            
             this.map = new maps.Map(node, mapConfig);
-          }
+
+            this.props.addresses.map( (add) => {
+              const marker = new google.maps.Marker({
+                position: {lat: add.lat, lng: add.lng},
+                map: this.map
+              });
+            })            
+      }
     }
 
     render() {
+      const style = {
+        width: '85vw',
+        height: '75vh'
+      }
       return (
-        <div ref='map'>
+        <div ref='map' style={style}>
           Loading map...
         </div>
       )
