ef4a704114a4eda355f305af9a5c202b86058b74
Unknown <diefrodriguezcha@unal.edu.co>
2018-05-03 02:22:49 -0500

    Dimensiones del mapa cambiadas, funciona con un marcador.

--- src/js/Map_Components/Container.js
+++ src/js/Map_Components/Container.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import GoogleApiComponent from 'google-maps-react/dist/GoogleApiComponent';
-import { Map } from "./Map.js";
+import ReactDOM from 'react-dom';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-export class Container extends React.Component {
-    render() {
-        const style = {
-            width: '100vw',
-            height: '100vh'
-          }
+export class MapContainer extends React.Component {
 
-        if (!this.props.loaded) {
-            return <div>Loading...</div>
-          }
-          return (
-            <div style={style}>
-                <Map google={this.props.google}
-                 zoom={14}/>
-            </div>
-          )
-    }
+    render() {      
+    return (
+      <div
+        style={{
+          position: "relative",
+          height: "500px",
+          width: "100%"
+        }}
+      >
+        < Map google={this.props.google}
+        zoom={14}
+        initialCenter={{
+          lat: 4.649737,
+          lng: -74.101081
+          }}
+        >
+
+          < Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />       
+        </ Map>
+      </div>
+    );
+  }
 }
 
-export default GoogleApiComponent({
-    apiKey: 'AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A'
-})(Container)
-
-
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAqD4Z3Cam8ZJqQr_v42hKjmQktYMq-27A'
+})(MapContainer)
