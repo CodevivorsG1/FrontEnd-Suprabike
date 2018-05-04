3eb5d48a795d9069e13bd8adf2531d43a41a7624
Dulfrey Hernández <wdhernandezh@unal.edu.co>
2018-05-03 21:09:05 -0500

    more statitics

--- src/js/DB_Components/Statistics.js
+++ src/js/DB_Components/Statistics.js
@@ -4,7 +4,7 @@ import AppHeaderComponent from '../AppHeaderComponent.js';
 import UploadZoneImages from '../Upload_Components/UploadZoneImages.js'
 import axios from 'axios';
 import store from '../store.js';
-import {BarChart , PieChart} from 'react-easy-chart';
+import {BarChart , PieChart ,ScatterplotChart } from 'react-easy-chart';
 
 class UserComponent extends React.Component {
   constructor(props){
@@ -73,6 +73,7 @@ class UserComponent extends React.Component {
   }
 
 
+     
 
   render(){
     
@@ -80,14 +81,47 @@ class UserComponent extends React.Component {
     //const numRows = membersToRender.length
     //console.info(membersToRender)
     const dataUser = [
-                    { x: 'Foros', y: this.state.nForums },
-                    { x: 'Participación', y: this.state.nComments },                    
+                    { x: 'Foros', y: this.state.nForums ,color: '#4dbfbf'},
+                    { x: 'Comentarios', y: this.state.nComments , color:'#2DA4A4' },                    
                   ];
     const dataBikes = [
       { key: 'Aluminio', value: 100 },
       { key: 'Acero', value: 200 },
       { key: 'Carbono', value: 50 }
     ]
+    const dataTransactions = []
+    for (var i = 0; i < this.state.nTransactions; i++) {
+      var transaction = {
+                      type: i,
+                      x: this.state.transactions[i].type_transaction,
+                      y: this.state.transactions[i].total_transaction
+                    }
+
+                    dataTransactions.push(transaction)
+    }
+    /*
+    const dataTransactions = [
+      {
+        type: 1,
+        x: 'Tue',
+        y: 10
+      },
+      {
+        type: 1,
+        x: 'Wed',
+        y: 20
+      },
+      {
+        type: 2,
+        x: 'Tue',
+        y: 30
+      },
+      {
+        type: 3,
+        x: 'Thu',
+        y: 40
+      }
+    ];*/
     if (this.state.isLoading){
 
       return(
@@ -112,11 +146,23 @@ class UserComponent extends React.Component {
 			                    </div>
 			                    <div class="col-sm-6 col-md-8">
 			                        <div id="myContainer"></div>
+                              <h2>Participación</h2>
                                   <BarChart
                                     axes
                                     height={250}
                                     width={400}
                                     data={dataUser}
+                                  />
+                                  <ScatterplotChart
+                                    data={dataTransactions}
+                                    axes
+                                    axisLabels={{x: 'Tipo', y: 'Valor'}}
+                                    dotRadius={6}
+                                    width={480}
+                                    height={270}
+                                    grid
+                                    xType="text"
+                                     
                                   />
 			                    </div>
 			                </div>
@@ -160,6 +206,34 @@ class UserComponent extends React.Component {
                                     }}
                                     data={dataBikes}
                                   />
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          
+      </div>
+
+      );}else if (this.state.type == '3'){
+    return (
+      <div class="container">
+          <div class="row">
+              <div class="col-xs-12 col-sm-6 col-md-6">
+                  <div class="well well-sm">
+                      <div class="row">
+                          <div class="col-sm-6 col-md-4">
+                              <label for="exampleFormControlSelect1">Seleccione tipo de estadisticas</label>
+                                <select class="form-control" onChange={this.change}  id="exampleFormControlSelect1">
+                                  <option value="1">Usuario</option>
+                                  <option value="2">Bicicletas</option>
+                                  <option value="3">Técnicos</option>
+                                  
+                                  
+                                </select>
+                          </div>
+                          <div class="col-sm-6 col-md-8">
+                              <div id="myContainer"></div>
+                                  
                           </div>
                       </div>
                   </div>
